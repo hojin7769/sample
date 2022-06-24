d5474344676fbeb18466b5f4a272699957d8f7aa
admin <https://github.com/hojin7769/sample.git>
2022-06-24 10:11:14 +0900

    remove dao,mapper AND SqlSessionTemplate add

--- src/main/java/com/example/sample/service/impl/BoardServiceImpl.java
+++ src/main/java/com/example/sample/service/impl/BoardServiceImpl.java
@@ -3,7 +3,7 @@ package com.example.sample.service.impl;
 import java.util.List;
 import java.util.Map;
 
-import com.example.sample.mapper.BoardMapper;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,39 +12,40 @@ import com.example.sample.service.BoardService;
 
 @Service
 public class BoardServiceImpl implements BoardService {
+	private final static String _MAPPER_NAMESPACE = "com.example.sample.mapper.";
+
 	@Autowired
-	private BoardMapper boardMapper;
+	private SqlSessionTemplate sqlSessionTemplate;
 
 	@Override
 	public List<Map<String, Object>> list(Map<String, Object> map) {
-		return boardMapper.list();
+		return sqlSessionTemplate.selectList(_MAPPER_NAMESPACE + "BoardMapper.list", map);
 	}
 
 	@Override
 	public Map<String, Object> detail(Map<String, Object> map) {
-		return boardMapper.detail(map);
+		return sqlSessionTemplate.selectOne(_MAPPER_NAMESPACE + "BoardMapper.detail", map);
 	}
 
 	@Override
 	@Transactional
 	public Map<String, Object> insert(Map<String, Object> map) {
-		int rtn = boardMapper.insert(map);
+		int rtn = sqlSessionTemplate.insert(_MAPPER_NAMESPACE + "BoardMapper.insert", map);
 		return map;
 	}
 
 	@Override
 	@Transactional
 	public Map<String, Object> update(Map<String, Object> map) {
-		int rtn = boardMapper.update(map);
-//		boardDAO.exeception(map);
+		int rtn = sqlSessionTemplate.update(_MAPPER_NAMESPACE + "BoardMapper.update", map);
 		return map;
 	}
-	
+
 
 	@Override
 	@Transactional
 	public Map<String, Object> delete(Map<String, Object> map) {
-		int rtn = boardMapper.delete(map);
+		int rtn = sqlSessionTemplate.delete(_MAPPER_NAMESPACE + "BoardMapper.delete", map);
 		return map;
 	}
 
