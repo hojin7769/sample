cf87a49135145618aa14a26e949c3cad1bd0bfa7
admin <https://github.com/hojin7769/sample.git>
2022-06-24 09:56:56 +0900

    add BoardVO and change Api AND service

--- src/main/java/com/example/sample/api/BoardApi.java
+++ src/main/java/com/example/sample/api/BoardApi.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.example.sample.model.BoardVO;
 import org.apache.commons.collections4.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,7 +22,7 @@ import com.example.sample.service.BoardService;
 public class BoardApi {
 	
 	@Autowired
-	private BoardService boardService;  
+	private BoardService boardService;
 
 	@GetMapping("/test")
 	public Map test() {
@@ -32,33 +33,34 @@ public class BoardApi {
 	}
 
 	@PostMapping("/boardList")
-	public List<Map<String, Object>> list(@RequestBody(required = false) Map<String, Object> param) {
-		return boardService.list(param);
-	}	
+	public List<BoardVO> list(@RequestBody(required = false) BoardVO param) {
+		return boardService.list();
+	}
+
 
 	@PostMapping("/boardDetail")
-	public Map<String, Object> detail(@RequestBody Map<String, Object> param) {
+	public Map<String, Object> detail(@RequestBody BoardVO param) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("data", boardService.detail(param));
 		return map;
 	}
 
 	@PostMapping("/boardSave")
-	public Map<String, Object> save(@RequestBody Map<String, Object> param) {
+	public Map<String, Object> save(@RequestBody BoardVO param) {
 
-		Integer no_seq = MapUtils.getInteger(param, "NO_SEQ");
-		
+//		Integer no_seq = MapUtils.getInteger(param, "NO_SEQ");
+
 		Map<String, Object> map = new HashMap<String, Object>();
-		if (no_seq != null && no_seq > 0) {
+		if (param.getNO_SEQ() > 0) {
 			map.put("data", boardService.update(param));
 		} else {
 			map.put("data", boardService.insert(param));
 		}
 		return map;
 	}
-	
+
 	@PostMapping("/boardDelete")
-	public Map<String, Object> delete(@RequestBody Map<String, Object> param) {
+	public Map<String, Object> delete(@RequestBody BoardVO param) {
 
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("data", boardService.delete(param));
