644269c7b1ea80abccac89385323dc548d84957a
admin <https://github.com/hojin7769/sample.git>
2022-06-24 09:36:04 +0900

    dao 없는 mapper

--- src/main/java/com/example/sample/service/impl/BoardServiceImpl.java
+++ src/main/java/com/example/sample/service/impl/BoardServiceImpl.java
@@ -3,39 +3,39 @@ package com.example.sample.service.impl;
 import java.util.List;
 import java.util.Map;
 
+import com.example.sample.mapper.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.sample.dao.BoardDAO;
 import com.example.sample.service.BoardService;
 
 @Service
 public class BoardServiceImpl implements BoardService {
 	@Autowired
-	private BoardDAO boardDAO;
+	private BoardMapper boardMapper;
 
 	@Override
 	public List<Map<String, Object>> list(Map<String, Object> map) {
-		return boardDAO.list(map);
+		return boardMapper.list();
 	}
 
 	@Override
 	public Map<String, Object> detail(Map<String, Object> map) {
-		return boardDAO.detail(map);
+		return boardMapper.detail(map);
 	}
 
 	@Override
 	@Transactional
 	public Map<String, Object> insert(Map<String, Object> map) {
-		int rtn = boardDAO.insert(map);
+		int rtn = boardMapper.insert(map);
 		return map;
 	}
 
 	@Override
 	@Transactional
 	public Map<String, Object> update(Map<String, Object> map) {
-		int rtn = boardDAO.update(map);
+		int rtn = boardMapper.update(map);
 //		boardDAO.exeception(map);
 		return map;
 	}
@@ -44,7 +44,7 @@ public class BoardServiceImpl implements BoardService {
 	@Override
 	@Transactional
 	public Map<String, Object> delete(Map<String, Object> map) {
-		int rtn = boardDAO.delete(map);
+		int rtn = boardMapper.delete(map);
 		return map;
 	}
 
