9b0d793ef0c19e05a04a39762b255bd40c5030ac
hohin <sungsill150@gmail.com>
2022-06-28 13:44:52 +0900

    add vue3 vite 뷰3 포함 기본 파일 커밋

--- src/main/java/com/example/sample/api/BoardApi.java
+++ src/main/java/com/example/sample/api/BoardApi.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:3000")
 public class BoardApi {
 
 	@Autowired
